17606e368d5028f6ebcb4fc9d341e8f9680ab167
ConnorUdal0 <udalizzer@gmail.com>
2025-10-29 17:54:54 -0600

    listview

--- app/src/main/java/com/example/matrix_events/activities/EventSearchActivity.java
+++ app/src/main/java/com/example/matrix_events/activities/EventSearchActivity.java
@@ -1,33 +1,68 @@
 package com.example.matrix_events.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.matrix_events.R;
+import com.example.matrix_events.entities.Event;
 import com.example.matrix_events.fragments.NavigationBarFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.matrix_events.managers.EventManager;
+import com.example.matrix_events.mvc.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventSearchActivity extends AppCompatActivity {
+
+    private ListView eventListView;
+    private ArrayAdapter<String> adapter;
+    private List<String> eventNames = new ArrayList<>();
+    private View eventView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_event_search);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        eventListView = findViewById(R.id.event_list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, eventNames);
+        eventListView.setAdapter(adapter);
+
+        eventView = new View() {
+            @Override
+            public void update() {
+                runOnUiThread(() -> {
+                    eventNames.clear();
+                    for (Event event : EventManager.getInstance().getEvents()) {
+                        eventNames.add(event.getName());
+                    }
+                    adapter.notifyDataSetChanged();
+                });
+            }
+        };
+
+        EventManager.getInstance().addView(eventView);
+
         getSupportFragmentManager().beginTransaction()
                 .setReorderingAllowed(true)
                 .replace(R.id.navigation_bar_fragment, NavigationBarFragment.newInstance(R.id.nav_event_search))
                 .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (eventView != null) {
+            eventView.update();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventManager.getInstance().removeView(eventView);
     }
 }
