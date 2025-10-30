70f74681f195b62d0d82dc24d8d4bd19145db982
ConnorUdal0 <udalizzer@gmail.com>
2025-10-30 16:49:14 -0600

    done

--- app/src/main/java/com/example/matrix_events/activities/EventSearchActivity.java
+++ app/src/main/java/com/example/matrix_events/activities/EventSearchActivity.java
@@ -1,6 +1,8 @@
 package com.example.matrix_events.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -8,9 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.matrix_events.R;
 import com.example.matrix_events.entities.Event;
+import com.example.matrix_events.fragments.EventDetailFragment;
 import com.example.matrix_events.fragments.NavigationBarFragment;
 import com.example.matrix_events.managers.EventManager;
-import com.example.matrix_events.mvc.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,7 @@ public class EventSearchActivity extends AppCompatActivity {
     private ListView eventListView;
     private ArrayAdapter<String> adapter;
     private List<String> eventNames = new ArrayList<>();
-    private View eventView;
+    private com.example.matrix_events.mvc.View eventView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,18 @@ public class EventSearchActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, eventNames);
         eventListView.setAdapter(adapter);
 
-        eventView = new View() {
+        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Event clickedEvent = EventManager.getInstance().getEvents().get(position);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main, EventDetailFragment.newInstance(clickedEvent.getId()))
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        eventView = new com.example.matrix_events.mvc.View() {
             @Override
             public void update() {
                 runOnUiThread(() -> {
--- app/src/main/java/com/example/matrix_events/fragments/EventDetailFragment.java
+++ app/src/main/java/com/example/matrix_events/fragments/EventDetailFragment.java
@@ -1,6 +1,61 @@
 package com.example.matrix_events.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.matrix_events.R;
+import com.example.matrix_events.entities.Event;
+import com.example.matrix_events.managers.EventManager;
+
 public class EventDetailFragment extends Fragment {
+
+    //idk whats happening here this is just temp
+
+    private static final String ARG_EVENT_ID = "event_id";
+
+    private String eventId;
+
+    public static EventDetailFragment newInstance(String eventId) {
+        EventDetailFragment fragment = new EventDetailFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_EVENT_ID, eventId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            eventId = getArguments().getString(ARG_EVENT_ID);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Event event = EventManager.getInstance().getEvent(eventId);
+
+        if (event != null) {
+            TextView eventName = view.findViewById(R.id.event_details_title_placeholder);
+            TextView eventDescription = view.findViewById(R.id.event_description_placeholder);
+
+            eventName.setText(event.getName());
+            eventDescription.setText(event.getDescription());
+        }
+    }
 }
