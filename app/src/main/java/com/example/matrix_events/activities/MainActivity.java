2dbd8c9163f5351db4d6eebc8868a32ec206d14e
albertganut <albert.ganut@gmail.com>
2025-11-02 01:14:40 -0600

    Implemented device ID check for login button

--- app/src/main/java/com/example/matrix_events/activities/MainActivity.java
+++ app/src/main/java/com/example/matrix_events/activities/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.matrix_events.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +17,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.matrix_events.R;
 import com.example.matrix_events.fragments.SignUpFragment;
-
+import com.example.matrix_events.managers.ProfileManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ProfileManager profileManager = ProfileManager.getInstance();
 
         Button loginButton = findViewById(R.id.login_button);
         Button signUpButton = findViewById(R.id.signup_button);
@@ -45,9 +50,18 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), EventSearchActivity.class);
-                startActivity(intent);
-                overridePendingTransition(0,0);
+                String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); // getting the device ID
+
+                if (profileManager.doesProfileExist(deviceId)) { // if the device ID exists in the database
+                    Log.d("Login", "Account accessed successfully using device ID: " + deviceId);
+                    Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), EventSearchActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                } else { // if the device ID does not exist in the database
+                    Log.d("Login", "You do not have an account yet. Please sign up first!");
+                    Toast.makeText(MainActivity.this, "You do not have an account yet. Please sign up first!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
