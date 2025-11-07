f9b258205a0d4749fcb6cc751a754781f41eed6d
Connor McRae <cjmcrae@ualberta.ca>
2025-11-07 16:00:08 -0700

    images on event details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     // Core testing dependencies
     testImplementation("junit:junit:4.13.2")
